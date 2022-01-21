a991e9ad9a4b826742dbd75addbbd339d07932b3
Dr.BaBoom <yurababu2000@yahoo.com>
2022-01-21 19:26:48 +0300

    Initial Commit

---
+++ DegreeWork_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DegreeWork_1
+//
+//  Created by Yura on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
